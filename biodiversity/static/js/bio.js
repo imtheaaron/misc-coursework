cbfd6f4da0d3fb777cfee926418df1956d16c373
Aaron Burke <35472931+imtheaaron@users.noreply.github.com>
2018-06-13 15:33:53 -0700

    added bubble plot and metadata box

--- biodiversity/static/js/bio.js
+++ biodiversity/static/js/bio.js
@@ -3,8 +3,8 @@ function init() {
     selector();    
 
     // create the initial pie chart
-    pie_url = "/samples/BB_940"
-    Plotly.d3.json(pie_url, function(error,response) {
+    url = "/samples/BB_940"
+    Plotly.d3.json(url, function(error,response) {
         var data = response;
         console.log(data);
         otus = data.otu_ids;
@@ -26,6 +26,7 @@ function init() {
 
         Plotly.plot("pie", pie_data, layout);
 
+        // add hovertext to both charts
         Plotly.d3.json("/otu", function(error,response) {
             var descriptions = response;
             var desc_list = [];
@@ -34,8 +35,40 @@ function init() {
                 desc_list.push(desc);
             };
             var PIE = document.getElementById("pie");
+            var BUBBLE = document.getElementById("bubble");
             Plotly.restyle(PIE, "hovertext", [desc_list])
+            Plotly.restyle(BUBBLE, "hovertext", [descriptions])
         })
+        
+        // create the bubble chart
+        var bubble_data = [{
+            x: otus,
+            y: values,
+            mode: "markers",
+            marker: {
+                size: values
+            }
+        }];
+
+        var bubble_layout = {
+            title: "Sample Values for Each OTU",
+            showlegend: false,
+            height: 500,
+            width: 1000,
+        };
+
+        Plotly.newPlot("bubble", bubble_data, bubble_layout);
+    });
+
+    meta_url = "/mdata/BB_940";
+    Plotly.d3.json(meta_url, function(error, response) {
+        sample_meta = response;
+        Plotly.d3.select(".card-body").append("P").text("AGE: " + sample_meta.AGE);
+        Plotly.d3.select(".card-body").append("P").text("BBTYPE: " + sample_meta.BBTYPE);
+        Plotly.d3.select(".card-body").append("P").text("ETHNICITY: " + sample_meta.ETHNICITY);
+        Plotly.d3.select(".card-body").append("P").text("GENDER: " + sample_meta.GENDER);
+        Plotly.d3.select(".card-body").append("P").text("LOCATION: " + sample_meta.LOCATION);
+        Plotly.d3.select(".card-body").append("P").text("SAMPLEID: " + sample_meta.SAMPLEID);
     })
 }
 
@@ -76,14 +109,32 @@ function update_plots(sample_id) {
         values = data.sample_values;
         values10 = values.slice(0,10);
 
-        var pie_data = [{
-            values: values10,
-            labels: otu10,
-        }];
+        // var pie_data = [{
+        //     values: values10,
+        //     labels: otu10,
+        // }];
 
         var PIE = document.getElementById("pie");
         Plotly.restyle(PIE, "values", [values10]);
         Plotly.restyle(PIE, "labels", [otu10]);
+
+        var bubble_data = [{
+            x: otus,
+            y: values,
+            mode: "markers",
+            marker: {
+                size: values
+            }
+        }];
+
+        var bubble_layout = {
+            title: "Sample Values for Each OTU",
+            showlegend: false,
+            height: 500,
+            width: 1000,
+        };
+
+        Plotly.newPlot("bubble", bubble_data, bubble_layout);
 
         Plotly.d3.json("/otu", function(error,response) {
             var descriptions = response;
@@ -93,8 +144,24 @@ function update_plots(sample_id) {
                 desc_list.push(desc);
             };
             var PIE = document.getElementById("pie");
-            Plotly.restyle(PIE, "hovertext", [desc_list])
+            Plotly.restyle(PIE, "hovertext", [desc_list]);
+            var BUBBLE = document.getElementById("bubble");
+            Plotly.restyle(BUBBLE, "hovertext", [descriptions]);
         })
+    })
+
+    // update the MetaData box
+    var meta_url = "/mdata/" + String(sample_id);
+    Plotly.d3.json(meta_url, function(error, response) {
+        sample_meta = response;
+        Plotly.d3.select(".card-body").html("");
+        Plotly.d3.select(".card-body").append("h4").text("Sample MetaData");
+        Plotly.d3.select(".card-body").append("P").text("AGE: " + sample_meta.AGE);
+        Plotly.d3.select(".card-body").append("P").text("BBTYPE: " + sample_meta.BBTYPE);
+        Plotly.d3.select(".card-body").append("P").text("ETHNICITY: " + sample_meta.ETHNICITY);
+        Plotly.d3.select(".card-body").append("P").text("GENDER: " + sample_meta.GENDER);
+        Plotly.d3.select(".card-body").append("P").text("LOCATION: " + sample_meta.LOCATION);
+        Plotly.d3.select(".card-body").append("P").text("SAMPLEID: " + sample_meta.SAMPLEID);
     })
 }
 
