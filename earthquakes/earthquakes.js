6b23b03dffcc928887bffe05bb20f955500418af
Aaron Burke <35472931+imtheaaron@users.noreply.github.com>
2018-06-25 17:03:10 -0700

    initial commit

---
+++ earthquakes/earthquakes.js
@@ -0,0 +1,8 @@
+//Dependencies
+
+
+//need stuff:
+// mapbox, leaflet, using a geojson
+
+// url for all earthquakes over the past 7 days:
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
